9aaadc11a8ae438c4d3513f2c6ec3f488004288b
Allen_zx <zhangxu_iii@163.com>
2025-07-09 17:56:45 +0800

    feat: init

---
+++ src/views/login/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>login</div>
+</template>
+<script lang="ts" setup>
+defineOptions({ name: 'LoginIndex' })
+import { useUserStore } from '@/store/user'
+const userStore = useUserStore()
+console.log(userStore)
+</script>
+<style lang="less" scoped></style>
