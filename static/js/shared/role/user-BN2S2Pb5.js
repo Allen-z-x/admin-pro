548445eeabf5bd532ad7be5a8dc7f4a13f3f61b5
Allen-z-x <Allen-z-x@users.noreply.github.com>
2025-07-10 08:23:24 +0000

    deploy: c315c8593a226401d315cb559b66f6d1bcd82047

--- static/js/shared/role/user-BN2S2Pb5.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"../../common-DRkHVigx.js";const s=async(t={})=>e({},"/getRoleList",t);export{s as g};
---
+++ static/js/shared/role/user-CI7eup2_.js
@@ -0,0 +1,1 @@
+import{g as e}from"../../common-DPZ3XbQ7.js";const s=async(t={})=>e({},"/getRoleList",t);export{s as g};
