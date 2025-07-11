65868cb7157f24dad81c45f52c6b7ec8dcf524d2
Allen_zx <zhangxu_iii@163.com>
2025-07-11 13:36:42 +0800

    feat: 修改配置

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,9 +1,19 @@
 /// <reference types="vite/client" />
+
 interface ImportMetaEnv {
   // 我们每次添加完新的环境变量就在此添加一次ts类型
   // 这样我们就能在使用 import.meta.env 时获得ts语法提示
   readonly VITE_APP_API_BASEURL: string
+  readonly VITE_APP_MOCK_BASEURL: string
 }
+
 interface ImportMeta {
   readonly env: ImportMetaEnv
+}
+
+// Vue 组件类型声明
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
